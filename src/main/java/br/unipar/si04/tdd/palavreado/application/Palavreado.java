f8ed7ab332ad772adf168bc0b7a24d5961340767
nathabonfim59 <dev@nathabonfim59.com>
2023-11-22 20:49:35 -0300

    Refactor word counting logic and add character counting method

--- src/main/java/br/unipar/si04/tdd/palavreado/application/Palavreado.java
+++ src/main/java/br/unipar/si04/tdd/palavreado/application/Palavreado.java
@@ -13,7 +13,18 @@ public class Palavreado {
         }
 
         String[] words = text.split(" ");
+
+        // desconsidera espaços em branco
+        words = java.util.Arrays.stream(words).map(s -> s.trim()).toArray(String[]::new);
+
+        // remove palavras vazias
+        words = java.util.Arrays.stream(words).filter(s -> (s != null && s.length() > 0)).toArray(String[]::new);
+
         return words.length;
+    }
+
+    public Object countCharacters() {
+        return text.length();
     }
     
 }
